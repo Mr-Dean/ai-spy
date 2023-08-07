228c43ddec0abcaa60dd849fb5f256718570ea26
Dean Arnold <deanarnold0@gmail.com>
2023-08-07 10:50:57 +0100

    created basic layout

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import './header.styles.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h1>aiSpy</h1>
+        </div>
+            
+    )
+}
+
+export default Header;
---
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -0,0 +1,13 @@
+const ImageLinkForm = () => {
+    return (
+       <div>
+        <p>Give me an image and I will tell you what's in it.</p>
+        <div>
+            <input type='text' placeholder="Paste a link to an image here..."/> 
+            <button>Submit</button>
+        </div>
+       </div>
+    )
+}
+
+export default ImageLinkForm;
